75feb6be982c18e0d154a51228ca8d2a78ca315d
Hrishikesh <hrishi.halesh.18@gmail.com>
2026-07-17 22:35:15 +0530

    fix: reconfigure CORS connection Vercel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(cors({ 
+    credentials: true, 
+    origin: [
+    'http://localhost:5173', 
+    'https://ems-projecr-seven.vercel.app'
+  ], }));
 
 // Routing Endpoints Mounted Cleanly
 app.use('/api/auth', authRoutes);
